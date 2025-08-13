086f3f18de37105eeff4ecb74b3877eaa0d4c000
David Gutiérrez <dvdgp93@gmail.com>
2025-08-14 00:52:12 +0200

    instalaciones

--- admin/assets/js/center.js
+++ admin/assets/js/center.js
@@ -152,7 +152,7 @@ function renderInstallations() {
                         <svg width="14" height="14" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                         </svg>
-                        ${installation.total_actividades || 0} actividades
+                        Actividades: ${installation.actividades_activas ?? installation.total_activas ?? 0} activas, ${installation.actividades_programadas ?? installation.total_programadas ?? 0} programadas, ${installation.actividades_finalizadas ?? installation.total_finalizadas ?? 0} finalizadas
                     </span>
                 </div>
             </div>
