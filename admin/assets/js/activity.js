8679dcb9b556e96d2fe3026a68358b51fac30447
David Gutiérrez <dvdgp93@gmail.com>
2025-08-18 11:39:24 +0200

    Página actividad + JS 6

--- admin/assets/js/activity.js
+++ admin/assets/js/activity.js
@@ -81,14 +81,6 @@ function renderParticipants() {
           <div class="center-header">
             <h3 class="center-name">${escapeHtml((p.apellidos || '') + ', ' + (p.nombre || ''))}</h3>
           </div>
-          <div class="center-details">
-            <span class="center-stat">
-              <svg width="14" height="14" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
-              </svg>
-              ID: ${Number(p.id) || ''}
-            </span>
-          </div>
         </div>
         <div class="center-actions">
           <div class="dropdown" onclick="event.stopPropagation()">
